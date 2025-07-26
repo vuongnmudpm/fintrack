4b2d27bbb019f0857fab58bc570c0417d0ddce7b
vuongnm10 <76800722+vuongnmudpm@users.noreply.github.com>
2025-07-26 08:58:50 +0700

    fix: cập nhật chức năng lấy tất cả danh mục Lấy tất cả danh mục bao gồm tên, loại, mô tả,... Closes

--- src/main/java/com/vuongnm/fintrack/controller/CategoryController.java
+++ src/main/java/com/vuongnm/fintrack/controller/CategoryController.java
@@ -2,22 +2,25 @@ package com.vuongnm.fintrack.controller;
 
 import com.vuongnm.fintrack.entity.Categories;
 import com.vuongnm.fintrack.repository.CategoryRepository;
+import com.vuongnm.fintrack.service.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
 @AllArgsConstructor
 public class CategoryController {
-    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
 
     //get all categories
     public ResponseEntity<List<Categories>> getAllCategories() {
-        List<Categories> categories = categoryRepository.findAll();
+        List<Categories> categories = categoryService.getAllCategories();
         return ResponseEntity.ok(categories);
     }
-}           
+}
